97e8d54c6e7a17771d08887302a9a7155c19fff1
Brandon Witt <bwitt2@uwo.ca>
2015-04-14 20:25:07 -0400

    added twiml

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 # spread the cuteness...
+
 import webapp2
 import random
 from google.appengine.ext import ndb
@@ -7,20 +8,44 @@ from google.appengine.ext import ndb
 class Pictures(ndb.Model):
   picURL = ndb.StringProperty()
 
-class getPic(webapp2.RequestHandler):
-    def post(self):
-        counter = 0
-        ndb_pics = Pictures.query()
-        rand = random.randint(0, ndb_pics.count()-1)
+class User(ndb.Model):
+  number = ndb.StringProperty()
+
+def twiml(message, has_media, media_url):
+  if has_media:
+    return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Message>"+message+"<Media>"+str(media_url)+"</Media></Message></Response>"
+  else:
+    return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Message>"+message+"</Message></Response>"
+
+def findPictureURL():
+  counter = 0
+  ndb_pics = Pictures.query()
+  rand = random.randint(0, ndb_pics.count()-1)
         
-        for pic in ndb_pics:
-          if(counter == rand):
-            self.response.headers['Content-Type'] = 'text/xml'
-            #self.response.write(twiml(pic.picURL))
-            self.response.write(self.request.get('content'))
-            break
-          counter += 1
+  for pic in ndb_pics:
+    if(counter == rand):
+      return pic.picURL
+    counter += 1
+  return "no pic"
 
-ROUTES = [webapp2.Route('/getPic', getPic, handler_method='POST')]
+class twilioCalling(webapp2.RequestHandler):
+    
+    def post(self):
 
-application = webapp2.WSGIApplication(ROUTES, debug = True)
+      self.response.headers['Content-Type'] = 'text/xml'
+      
+      sender = str(self.request.get("From"))
+      new_user = User.query(User.number == sender).count() == 0
+      wants_cute = "#cuteness" in str(self.request.get("Body"))
+
+      if new_user:
+        User(number=sender).put()
+        self.response.write(twiml("Welcome to the cuteness! Just include #cuteness for a cuter day! Here's your first cuteness! :)", True, findPictureURL()))
+      elif wants_cute:
+        self.response.write(twiml("Spread the cuteness! :)", True, findPictureURL()))
+      else:
+        self.response.write(twiml("Thanks for the text! Add #cuteness in your text for some cuteness! :)", False, ""))
+
+
+application = webapp2.WSGIApplication( [('/', twilioCalling)], debug = True)
+
